b28068ff667478efb4e95984b1c05e69a52467cb
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-11-16 02:51:31 +0530

    first commit

---
+++ pipeline_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+AccountName="personalwebsitestorage"
+RG="PersonalWebsiteRG"
+ProfileName="PersonalWebsiteCDN"
+EndPoint="PersonalWebsiteEndpoint"
+
+echo "--------------------------------------------------------"
+echo "|   Removing previous content from CDN edge locations  |"
+echo "--------------------------------------------------------"
+
+az cdn endpoint purge \
+      --name $EndPoint \
+      --profile-name $ProfileName \
+      --resource-group $RG \
+      --content-paths "/*"
+
+
+echo "--------------------------------------------------------"
+echo "|           Uploading to Azure Storage Account         |"
+echo "--------------------------------------------------------"
+
+Connection_String=$(az storage account show-connection-string --name $AccountName --resource-group $RG --query connectionString -o tsv)
+az storage blob upload-batch \
+  -d "\$web" \
+  -s "website" \
+  --connection-string $Connection_String \
+  --overwrite \
+  --pattern "*"
+
